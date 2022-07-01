145c0df1488bf568be26a7dad4ee79b77d8246b8
Akbar <tes@gmail.com>
2022-07-01 09:41:41 +0700

    Add sidebar

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Akbarazy | Profile</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="static/css/font-awesome.min.css">
+    <link rel="stylesheet" href="static/css/style.css">
+</head>
+
+<body>
+    <!-- section navbar -->
+
+    <ul class="nav flex-column bg-dark fixed-left display-inline-block">
+        <li class="nav-item pt-2 text-center nav-profile">
+            <a class="nav-link border-bottom pb-3 pe-4 text-white fs-3 fw-semibold nav-profile" aria-current="page" href="#">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                <span>Profile</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white padding-left" href="#">
+                <i class="fa fa-home" aria-hidden="true"></i>
+                <span>Home</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-white padding-left" href="#">
+                <i class="fa fa-commenting" aria-hidden="true"></i>
+                <span>Massage</span>
+            </a>
+        </li>
+
+        <li class="nav-item fixed-bottom border-top">
+            <a class="nav-link text-white padding-left" href="#">
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+                <span>Logout</span>
+            </a>
+        </li>
+    </ul>
+
+    <!-- end navbar -->
+
+
+    <!-- section header -->
+
+
+
+    <!-- end header -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
